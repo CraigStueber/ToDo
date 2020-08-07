80d873482fb8af44e6e479e9be357232b278a696
CraigStueber <craig@kurbmedia.com>
2020-08-06 22:41:23 -0400

    working

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ function App(){
   const todoNameRef = useRef()
 
   function handleAddTodo(e) {
-
-
+      const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos =>{
+      return [...prevTodos, { id: 1, name: name,complete: false}]
+    })
+    todoNameRef.current.value = null
   }
 
 
